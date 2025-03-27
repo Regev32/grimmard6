1f7715c4baf6e0ce9f70b9b09465ee9ed8d1a504
yehezkr7 <regevel2006@gmail.com>
2025-03-27 14:06:11 +0200

    Now works for 9 loci.

--- setup/produce_example_graph_file.py
+++ setup/produce_example_graph_file.py
@@ -2,30 +2,11 @@ import os
 import pickle
 import shutil
 import gzip
-from graph_generation.generate_hpf import produce_hpf
-from generate_config_dict import generate_dict_config
-from grim import grim
+from RunGrim import run_original_grim
 
 # Step 1: Create HPF File
 config_file = '../app/conf/conf.json'
-produce_hpf(conf_file=config_file)
-print("1. Produced: output_new/hpf.csv")
-
-# Step 2: Create nodes/edges
-grim.graph_freqs(config_file)
-print("2. Produced nodes and edges: output_new/csv")
-
-# Step 3: Create networkx Graph
-dict_config = generate_dict_config(config_file)
-# Create a graph instance based on your configuration
-graph = grim.graph_instance(dict_config)
-
-# Pickle it out
-# Assuming 'graph' is your graph instance, save it as a pickle
-os.makedirs("../app/data", exist_ok=True)
-with open('../app/data/graph.pkl', 'wb') as pickle_file:
-    pickle.dump(graph, pickle_file)
-print("3. Produced Whole Graph: app/data/graph.pkl")
+run_original_grim(config_file, hap_pop_pair=True, Producehpf=True, dominant3=True)
 
 # Generate the freq dictionary file
 os.makedirs("../app/data/freqs_dicts", exist_ok=True)
